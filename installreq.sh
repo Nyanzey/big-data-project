782664e0760b71b102acbe8aa0a655a968085887
BrnFez <bruno.fernandez@ucsp.edu.pe>
2024-11-21 15:24:34 -0500

    added requirements

---
+++ installreq.sh
@@ -0,0 +1,14 @@
+sudo python3 -m pip install opencv-python
+sudo python3 -m pip install scikit-learn
+sudo python3 -m pip install numpy
+sudo python3 -m pip install webcolors
+sudo python3 -m pip install matplotlib
+sudo python3 -m pip install torch
+sudo python3 -m pip install Pillow
+sudo python3 -m pip install transformers
+sudo python3 -m pip install torchvision
+sudo python3 -m pip install scipy
+sudo python3 -m pip install boto3
+sudo python3 -m pip install python-dateutil==2.8.2
+sudo python3 -m pip install psutil pandas==2.2.3 py-cpuinfo ultralytics-thop==2.0.12 seaborn==0.13.2 tzdata
+sudo python3 -m pip install ultralytics --no-deps
